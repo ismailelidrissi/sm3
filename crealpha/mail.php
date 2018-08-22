2079bef326cc989b90bed7bc90e08a30c3135199
Ismail <ismail.elidrissi.b@gmail.com>
2018-08-22 14:41:30 +0100

    Update mail.php

--- crealpha/mail.php
+++ crealpha/mail.php
@@ -3,16 +3,11 @@
 //Taking all values
 $name 		= $_POST['name'];
 $email 		= $_POST['email'];
-$phone 		= $_POST['phone'];
 $msg 		= $_POST['msg'];
-$output 	= "Name: ".$name."\n\nEmail: ".$email."\n\nPhone: ".$phone."\n\nMessage: ".$msg;
+$output 	= "Name: ".$name."\n\nEmail: ".$email."\n\nMessage: ".$msg;
 
 $to 		= 'contact@crealphallc.com';
 $headers	= 'FROM: "'.$email.'"';
 
 $send		= mail($to, $name, $output."\n\n***This message has been sent from Crealpha", $headers);
-<<<<<<< HEAD
 ?>
-=======
-?>
->>>>>>> d6c69c7a09b1582e65a0bbd2a7fd3a236998e1e2
